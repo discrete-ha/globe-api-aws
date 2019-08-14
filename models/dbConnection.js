ea44d59f9e3d699cdbf587f5f8c103f242a7140c
discrete-ha <discreteb@gmail.com>
2019-08-14 12:39:59 +0900

    cache

--- models/dbConnection.js
+++ models/dbConnection.js
@@ -1,11 +1,15 @@
-var memjs = require('memjs');
-let memcacheServer = require(process.cwd()+'/config.json').memcache_server;
+let Memcached = require('memcached-elasticache');
 
-var client = memjs.Client.create(memcacheServer, {
-  failover: true,
-  timeout: 1,
-  keepAlive: true
-});
+
+let memcacheServer = "globe-api-topics.vckbjt.cfg.apne1.cache.amazonaws.com:11211";
+
+// var client = memjs.Client.create(memcacheServer, {
+//   failover: true,
+//   timeout: 1,
+//   keepAlive: true
+// });
+
+let memcachedClient = new Memcached(memcacheServer, {});
 
 module.exports = function () {
   return {
