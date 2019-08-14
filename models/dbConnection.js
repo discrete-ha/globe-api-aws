6e346d59b0f40d3cc6ce5c90bd8752a1b1d45847
discrete-ha <discreteb@gmail.com>
2019-08-14 12:49:46 +0900

    typo

--- models/dbConnection.js
+++ models/dbConnection.js
@@ -2,14 +2,13 @@ let Memcached = require('memcached-elasticache');
 
 
 let memcacheServer = require(process.cwd()+'/config.json').memcache_server;
-
 let memcachedClient = new Memcached(memcacheServer, {});
 
 module.exports = function () {
   return {
 
     set: function (key, value) {
-		client.set(key, value, {expires:120}, function(err, val) {
+		memcachedClient.set(key, value, {expires:120}, function(err, val) {
 			if (err) {
 				console.log("err",err);
 			}
@@ -18,7 +17,7 @@ module.exports = function () {
 
     get: function (key, callback) {
     	try{
-    		client.get(key, function(err, val) {
+    		memcachedClient.get(key, function(err, val) {
 				return callback(err, val);
 			});
     	}catch(err){
